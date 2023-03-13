1d1509b8593be3237830df528cb978ac7eda8ae9
Omar D. Ramirez <omardramirez2002@gmail.com>
2023-03-12 21:46:49 -0500

    This commit adds a dark theme to project and will be changed to be linked to a toggle.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import StartPage from './page/StartPage';
 import MainLayoutPage from './page/MainLayoutPage';
+import ColorModeButton from './components/ThemeToggleButton';
 
+
+// const ColorModeContext = createContext({ toggleColorMode: () => {} });
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
+
+  // const theme = useTheme();
+  // const colorMode = React.useContext(ColorModeContext);
+
+
+
   return (
+
+    <ThemeProvider theme={darkTheme}>
+    <Box
+    sx ={{
+      display:'flex',
+      bgcolor: 'background.default',
+      color:'text.primary'
+    }}
+    >
+
     <BrowserRouter>
       <Routes>
         <Route path ="/" element = {StartPage()}/> 
-        <Route exact path ="/MainLayout/*" element = {MainLayoutPage()}/> 
+        <Route path ="/MainLayout/*" element = {MainLayoutPage()}/> 
+
       </Routes>
     </BrowserRouter>
+    <ColorModeButton />
+    </Box>
+    </ThemeProvider>
+
   );
 }
 
---
+++ client/src/components/ThemeToggleButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+
+function ColorModeButton(){
+    return(
+        <Button>Toggle</Button>
+    );
+
+};
+
+export default ColorModeButton;
