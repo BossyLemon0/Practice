c47e7775e547e36ca50d52053023ebd236dc0644
Omar D. Ramirez <omardramirez2002@gmail.com>
2023-03-14 05:55:10 -0500

    This commit taught me useMemo, useContext, and showed me how to use themes and styles.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,40 @@
-import React, { createContext, useContext } from 'react'
+import  { createContext, useContext, useState, useMemo } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import StartPage from './page/StartPage';
 import MainLayoutPage from './page/MainLayoutPage';
 import ColorModeButton from './components/ThemeToggleButton';
+import { sizing } from '@mui/system';
 
 
-// const ColorModeContext = createContext({ toggleColorMode: () => {} });
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
+  breakpoints:{
+    values:{
+
+    }
+  }
 });
 
 function App() {
 
-  // const theme = useTheme();
-  // const colorMode = React.useContext(ColorModeContext);
-
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
 
 
   return (
-
-    <ThemeProvider theme={darkTheme}>
       <Box
       sx ={{
         display:'flex',
         bgcolor: 'background.default',
-        color:'text.primary'
+        color:'text.primary',
+        width:'100%',
+        height:'100%'
       }}
       >
 
@@ -39,11 +45,55 @@ function App() {
 
         </Routes>
       </BrowserRouter>
-      <ColorModeButton />
+      <ColorModeButton props={{theme, colorMode}} />
       </Box>
-    </ThemeProvider>
+
 
   );
 }
 
-export default App;
+//s
+function ToggleColorWrapper(){
+  const [mode, setMode] = useState('light');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          ...(mode === 'light'
+          ? {
+            background:{
+              default: "blue",
+              paper: "blue"
+            }
+          }:{
+            background:{
+              default: "orange",
+              paper: "orange"
+            }
+          }
+          )
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App/>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default ToggleColorWrapper;
--- client/src/components/ThemeToggleButton.js
+++ client/src/components/ThemeToggleButton.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Button } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 
-function ColorModeButton(){
+function ColorModeButton(props){
+    const { theme, colorMode } = props.props
     return(
-        <Button>Toggle</Button>
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
     );
 
 };
