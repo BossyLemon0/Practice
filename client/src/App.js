0c2cb6fdfdd4893284db1f5eab4108f6b646e982
Omar D. Ramirez <omardramirez2002@gmail.com>
2023-03-15 22:19:40 -0500

    Changes the context to work with persistent storage using local storage. The change also documents how to use the hooks effectively.

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,28 @@
-import  { createContext, useContext, useState, useMemo } from 'react'
+import  { createContext, useState, useMemo, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import StartPage from './page/StartPage';
 import MainLayoutPage from './page/MainLayoutPage';
 import ColorModeButton from './components/ThemeToggleButton';
-import { sizing } from '@mui/system';
+// import { sizing } from '@mui/system';
+import { Box } from '@mui/material';
 
-
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-  breakpoints:{
-    values:{
-
-    }
-  }
-});
+//default value, used when the provider is not available, so really anything can be in the create context
+//The colorModeContext changes based on the value passes in by the Context provider
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 function App() {
 
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
-
   return (
-      <Box
+       <Box
       sx ={{
         display:'flex',
         bgcolor: 'background.default',
         color:'text.primary',
         width:'100%',
-        height:'100%'
+        height:'100%',
+        margin: 0,
+        padding: 0
       }}
       >
 
@@ -42,28 +30,38 @@ function App() {
         <Routes>
           <Route path ="/" element = {StartPage()}/> 
           <Route path ="/MainLayout/*" element = {MainLayoutPage()}/> 
-
         </Routes>
       </BrowserRouter>
-      <ColorModeButton props={{theme, colorMode}} />
+      <ColorModeButton />
       </Box>
-
 
   );
 }
 
-//s
+//
 function ToggleColorWrapper(){
-  const [mode, setMode] = useState('light');
+  //uses either what's in local storage or uses light
+  const [mode, setMode] = useState(() => localStorage.getItem('mode') || 'light');
+
+  //This memoizations stops any uneccessary re-renders of color mode occuring
+  //the colorMode uses a short hand for return, instead of ()=>{return()} it uses ()=>()
+  //the togglecolormode function switches the mode to the opposite of the last mode
+  //then after switching the state, it sets the new state to local storage so it persists
+  //the object containing toggle color is only rendered when the mode state is changed
+  //NOTE: removing mode from the dependency stops the button from firing after the first click.
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        const newMode = mode === 'light' ? 'dark' : 'light';
+        setMode(newMode);
+        localStorage.setItem('mode', newMode);
       },
     }),
-    [],
+    [mode],
   );
 
+  //Create theme returns a theme object.
+  //This theme is only chached and stored if mode changes states
   const theme = useMemo(
     () =>
       createTheme({
@@ -86,6 +84,10 @@ function ToggleColorWrapper(){
       }),
     [mode],
   );
+
+  useEffect(()=>{
+    console.log(theme.palette.mode)
+  }, [theme])
 
   return (
     <ColorModeContext.Provider value={colorMode}>
--- client/src/components/ThemeToggleButton.js
+++ client/src/components/ThemeToggleButton.js
@@ -1,11 +1,16 @@
-import React from "react";
+import { useContext } from 'react'
 import IconButton from '@mui/material/IconButton';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from '@mui/material/styles';
+import { ColorModeContext } from "../App";
 
 
-function ColorModeButton(props){
-    const { theme, colorMode } = props.props
+function ColorModeButton(){
+    const theme = useTheme();
+    const colorMode = useContext(ColorModeContext);
+
+    
     return(
         <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
